c340717fee280c003ffd0414599c75b85512a0d7
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2021-01-17 22:36:25 -0300

    Teste inicial bp

--- core/MLP.py
+++ core/MLP.py
@@ -2,16 +2,21 @@ import numpy as np
 
 class MLP :
 
-    def __init__ ( self, qtd_in, qtd_h, qtd_out ) :
+    def __init__ ( self, qtd_in, qtd_h, qtd_out, ni=0.001 ) :
         self.qtd_in  = qtd_in
         self.qtd_h   = qtd_h
         self.qtd_out = qtd_out
 
-        self.wh = np.random.random ( self.qtd_in + 1, self.qtd_h )
-        self.wo = np.random.random ( self.qtd_h + 1, self.qtd_out )
+        self.ni = ni
 
-    def treinar ( self, x, y, threshold=0.5 ) :
-        
+        self.wh = np.random.random (( self.qtd_in + 1, self.qtd_h ))
+        self.wo = np.random.random (( self.qtd_h + 1, self.qtd_out ))
+
+        print(self.wh.shape)
+        print(self.wo.shape)
+
+    def feed ( self, x ) :
+
         input_x = x.copy()
         input_x.append(1)
 
@@ -22,4 +27,40 @@ class MLP :
 
         O = sigmoid ( np.dot ( H, self.wo ))
 
-        print ( O )
+        return O
+
+    def treinar ( self, x, y, threshold=0.5 ) :
+        
+        input_x = x.copy()
+        input_x.append(1)
+
+        output_y = np.array ( y )
+
+        def sigmoid ( x ) :
+            return 1. / ( 1 + np.exp ( -x ))
+
+        H = sigmoid ( np.dot ( np.array ( input_x ), self.wh ))
+
+        H = np.append ( H, [1] )
+
+        O = sigmoid ( np.dot ( H, self.wo ))
+
+        erro = np.subtract( output_y, O )
+
+        classif = [ 0 if output <= threshold else 1 for output in O ]
+
+        erros_classif = np.subtract( output_y, np.array ( classif ))
+        erro_classif = np.sum ( erros_classif )
+
+        DO = O * ( np.ones ( len ( O )) - O ) * ( output_y - O )
+
+        DH = np.dot( H * ( np.ones ( len ( H )) - H ), np.dot ( DO, np.transpose ( self.wo )))
+
+        sh = self.ni * np.dot ( DH, np.array(input_x))   
+
+        print(sh.shape)
+
+        # self.wh = self.wh 
+        # self.wo = self.wo + self.ni * np.dot ( DO, H )   
+
+        return np.sum ( np.abs ( erro )), 0 if erro_classif == 0 else 1
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from core.MLP import MLP
+from random import shuffle
 
-mlp = MLP(2, 2, 2)
+EPOCHS = 200
 
+mlp = MLP(2, 2, 1)
+
+dataset = [
+    ([ 0, 0 ], [ 0 ]),
+    ([ 0, 1 ], [ 0 ]),
+    ([ 1, 0 ], [ 0 ]),
+    ([ 1, 1 ], [ 0 ])
+]
+
+for i in range ( EPOCHS ) :
+    erroAproxEpoca = 0
+    erroClassEpoca = 0
+    data = dataset
+    shuffle ( data )
+    for sample in data :
+        erro_aprox, erro_class = mlp.treinar ( sample[0], sample[1] )
+        erroAproxEpoca += erro_aprox
+        erroClassEpoca += erro_class
+    print(f"Época {i + 1} | Erro aprox: { erroAproxEpoca } | Erro class: { erroClassEpoca } ")
